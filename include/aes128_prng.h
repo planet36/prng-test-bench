647ed3383beeec7c71419726ceceab6a61d8c16d
Steven Ward <planet36@users.noreply.github.com>
2024-07-22 17:30:13 -0400

    Add aes128_prng.h

---
+++ include/aes128_prng.h
@@ -0,0 +1,143 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// AES128 PRNG
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <err.h>
+#include <immintrin.h>
+#include <unistd.h>
+
+/// Do \c _mm_aesenc_si128 \a N times on data \a a with key \a key
+/**
+\pre \a N must be at least \c 1.
+\param a the data
+\param key the key
+\param N the number of rounds of encryption to perform
+*/
+static __m128i
+aes128_mix(__m128i a, const __m128i key, const unsigned int N)
+{
+	for (unsigned int round = 0; round < N; ++round)
+	{
+		a = _mm_aesenc_si128(a, key);
+	}
+	return a;
+}
+
+/// Davies-Meyer single-block-length compression function that uses AES as the block cipher
+/**
+\sa https://en.wikipedia.org/wiki/One-way_compression_function#Davies%E2%80%93Meyer
+\pre \a N must be at least \c 1.
+\param H the previous hash value
+\param m the block of the message
+\param N the number of rounds of encryption to perform
+\return the next hash value
+*/
+static inline __m128i
+aes128_davies_meyer(const __m128i H, const __m128i m, const unsigned int N)
+{
+	return _mm_xor_si128(H, aes128_mix(H, m, N));
+}
+
+/// A PRNG that uses AES instructions
+/**
+* This does \em not satisfy the following:
+*  - named requirement \c UniformRandomBitGenerator
+*  - concept \c std::uniform_random_bit_generator
+* \sa https://en.cppreference.com/w/cpp/named_req/UniformRandomBitGenerator
+* \sa https://en.cppreference.com/w/cpp/numeric/random/uniform_random_bit_generator
+*/
+struct aes128_prng
+{
+	__m128i x;
+	__m128i c; // must be odd
+	__m128i key;
+};
+
+typedef struct aes128_prng aes128_prng;
+
+// getentropy will fail if more than 256 bytes are requested.
+static_assert(sizeof(aes128_prng) <= 256);
+
+/// Assign random bytes to the data members via \c getentropy.
+static void
+aes128_prng_reseed(aes128_prng* _this)
+{
+	if (getentropy(_this, sizeof(*_this)) < 0)
+	{ err(EXIT_FAILURE, "getentropy"); }
+
+	// Every odd integer is coprime with every power of 2.  Ensure c is odd.
+	_this->c =_mm_or_si128(_this->c, _mm_set_epi64x(1, 1));
+}
+
+/// Get the next PRNG output.
+
+static __m128i
+aes128_prng_next_mix_n1(aes128_prng* _this)
+{
+	const __m128i dst = aes128_mix(_this->x, _this->key, 1);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_mix_n2(aes128_prng* _this)
+{
+	const __m128i dst = aes128_mix(_this->x, _this->key, 2);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_mix_n3(aes128_prng* _this)
+{
+	const __m128i dst = aes128_mix(_this->x, _this->key, 3);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_mix_n4(aes128_prng* _this)
+{
+	const __m128i dst = aes128_mix(_this->x, _this->key, 4);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_dm_n1(aes128_prng* _this)
+{
+	const __m128i dst = aes128_davies_meyer(_this->x, _this->key, 1);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_dm_n2(aes128_prng* _this)
+{
+	const __m128i dst = aes128_davies_meyer(_this->x, _this->key, 2);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_dm_n3(aes128_prng* _this)
+{
+	const __m128i dst = aes128_davies_meyer(_this->x, _this->key, 3);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_dm_n4(aes128_prng* _this)
+{
+	const __m128i dst = aes128_davies_meyer(_this->x, _this->key, 4);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
