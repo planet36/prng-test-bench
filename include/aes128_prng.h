a9bc52cbdeb41b4685572144c0bc07b7dffa7b3c
Steven Ward <planet36@users.noreply.github.com>
2024-07-23 16:07:02 -0400

    Add functions that do aesdec, corresponding to ones that do aesenc

--- include/aes128_prng.h
+++ include/aes128_prng.h
@@ -34,6 +34,23 @@ aes128_enc_mix(__m128i a, const __m128i key, const unsigned int N)
 	return a;
 }
 
+/// Do \c _mm_aesdec_si128 \a N times on data \a a with key \a key
+/**
+\pre \a N must be at least \c 1.
+\param a the data
+\param key the key
+\param N the number of rounds of decryption to perform
+*/
+static __m128i
+aes128_dec_mix(__m128i a, const __m128i key, const unsigned int N)
+{
+	for (unsigned int round = 0; round < N; ++round)
+	{
+		a = _mm_aesdec_si128(a, key);
+	}
+	return a;
+}
+
 /// Davies-Meyer single-block-length compression function that uses AES as the block cipher
 /**
 \sa https://en.wikipedia.org/wiki/One-way_compression_function#Davies%E2%80%93Meyer
@@ -47,6 +64,21 @@ static inline __m128i
 aes128_enc_davies_meyer(const __m128i H, const __m128i m, const unsigned int N)
 {
 	return _mm_xor_si128(H, aes128_enc_mix(H, m, N));
+}
+
+/// Davies-Meyer single-block-length compression function that uses AES as the block cipher
+/**
+\sa https://en.wikipedia.org/wiki/One-way_compression_function#Davies%E2%80%93Meyer
+\pre \a N must be at least \c 1.
+\param H the previous hash value
+\param m the block of the message
+\param N the number of rounds of decryption to perform
+\return the next hash value
+*/
+static inline __m128i
+aes128_dec_davies_meyer(const __m128i H, const __m128i m, const unsigned int N)
+{
+	return _mm_xor_si128(H, aes128_dec_mix(H, m, N));
 }
 
 /// A PRNG that uses AES instructions
@@ -161,6 +193,70 @@ static __m128i
 aes128_prng_next_enc_dm_n4(aes128_prng* _this)
 {
 	const __m128i dst = aes128_enc_davies_meyer(_this->x, _this->key, 4);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_dec_mix_n1(aes128_prng* _this)
+{
+	const __m128i dst = aes128_dec_mix(_this->x, _this->key, 1);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_dec_mix_n2(aes128_prng* _this)
+{
+	const __m128i dst = aes128_dec_mix(_this->x, _this->key, 2);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_dec_mix_n3(aes128_prng* _this)
+{
+	const __m128i dst = aes128_dec_mix(_this->x, _this->key, 3);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_dec_mix_n4(aes128_prng* _this)
+{
+	const __m128i dst = aes128_dec_mix(_this->x, _this->key, 4);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_dec_dm_n1(aes128_prng* _this)
+{
+	const __m128i dst = aes128_dec_davies_meyer(_this->x, _this->key, 1);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_dec_dm_n2(aes128_prng* _this)
+{
+	const __m128i dst = aes128_dec_davies_meyer(_this->x, _this->key, 2);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_dec_dm_n3(aes128_prng* _this)
+{
+	const __m128i dst = aes128_dec_davies_meyer(_this->x, _this->key, 3);
+	_this->x = _mm_add_epi64(_this->x, _this->c);
+	return dst;
+}
+
+static __m128i
+aes128_prng_next_dec_dm_n4(aes128_prng* _this)
+{
+	const __m128i dst = aes128_dec_davies_meyer(_this->x, _this->key, 4);
 	_this->x = _mm_add_epi64(_this->x, _this->c);
 	return dst;
 }
