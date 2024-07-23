88eeeb2efc808945709b29b16bd14ae6549861d0
Steven Ward <planet36@users.noreply.github.com>
2024-07-23 15:14:12 -0400

    Remove penultimate variable and re-use result variable

--- include/aeslastrand.hpp
+++ include/aeslastrand.hpp
@@ -22,8 +22,8 @@ DEF_URBG_CLASS(aesenclastrand, __m128i, __uint128_t)
 {
 	const __m128i roundKey = _mm_set_epi64x(byteprimes[1], byteprimes[0]);
 	s = _mm_add_epi64(s, roundKey);
-	const __m128i penultimate = _mm_aesenc_si128(s, roundKey);
-	const __m128i result = _mm_aesenclast_si128(penultimate, roundKey);
+	__m128i result = _mm_aesenc_si128(s, roundKey);
+	result = _mm_aesenclast_si128(result, roundKey);
 	return union_128{.xmm = result}.u128;
 
 	//return result[0]; // failed at 2 GB
@@ -38,8 +38,8 @@ DEF_URBG_CLASS(aesdeclastrand, __m128i, __uint128_t)
 {
 	const __m128i roundKey = _mm_set_epi64x(byteprimes[1], byteprimes[0]);
 	s = _mm_add_epi64(s, roundKey);
-	const __m128i penultimate = _mm_aesdec_si128(s, roundKey);
-	const __m128i result = _mm_aesdeclast_si128(penultimate, roundKey);
+	__m128i result = _mm_aesdec_si128(s, roundKey);
+	result = _mm_aesdeclast_si128(result, roundKey);
 	return union_128{.xmm = result}.u128;
 
 	//return result[0]; // failed at 256 MB
--- include/aesrand.hpp
+++ include/aesrand.hpp
@@ -25,8 +25,8 @@ DEF_URBG_CLASS(aesencrand, __m128i, __uint128_t)
 {
 	const __m128i roundKey = _mm_set_epi64x(byteprimes[1], byteprimes[0]);
 	s = _mm_add_epi64(s, roundKey);
-	const __m128i penultimate = _mm_aesenc_si128(s, roundKey);
-	const __m128i result = _mm_aesenc_si128(penultimate, roundKey);
+	__m128i result = _mm_aesenc_si128(s, roundKey);
+	result = _mm_aesenc_si128(result, roundKey);
 	return union_128{.xmm = result}.u128;
 }
 
@@ -34,9 +34,8 @@ DEF_URBG_CLASS(aesdecrand, __m128i, __uint128_t)
 {
 	const __m128i roundKey = _mm_set_epi64x(byteprimes[1], byteprimes[0]);
 	s = _mm_add_epi64(s, roundKey);
-	//const __m128i penultimate = _mm_aesenc_si128(s, roundKey);
-	const __m128i penultimate = _mm_aesdec_si128(s, roundKey); // (SDW)
-	const __m128i result = _mm_aesdec_si128(penultimate, roundKey);
+	__m128i result = _mm_aesdec_si128(s, roundKey); // (SDW)
+	result = _mm_aesdec_si128(result, roundKey);
 	return union_128{.xmm = result}.u128;
 }
 #else
