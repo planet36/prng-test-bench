3052771bd8ccf402d0241a9f6b470cd4261e75b7
Steven Ward <planet36@users.noreply.github.com>
2024-07-23 22:24:34 -0400

    Add macro to create an AES PRNG class

--- include/aes128_prng.hpp
+++ include/aes128_prng.hpp
@@ -26,6 +26,28 @@ static_assert(std::signed_integral<__int128_t>);
 static_assert(std::integral<__uint128_t>);
 static_assert(std::unsigned_integral<__uint128_t>);
 
+#define DEF_AES_PRNG_CLASS(METHOD)                                                         \
+class aes128_prng_ ## METHOD                                                               \
+{                                                                                          \
+private:                                                                                   \
+	aes128_prng state;                                                                     \
+public:                                                                                    \
+	using result_type = __uint128_t;                                                       \
+	using seed_bytes_type = std::array<uint8_t, sizeof(__m128i)>;                          \
+	aes128_prng_ ## METHOD() { aes128_prng_reseed(&state); }                               \
+	aes128_prng_ ## METHOD(const seed_bytes_type& bytes) {                                 \
+		aes128_prng_reseed(&state);                                                        \
+		(void)std::memcpy(&state.x, bytes.data(), sizeof(__m128i)); }                      \
+	static constexpr result_type min() { return std::numeric_limits<result_type>::min(); } \
+	static constexpr result_type max() { return std::numeric_limits<result_type>::max(); } \
+	constexpr result_type operator()() { return next(); }                                  \
+	result_type next() {                                                                   \
+		const __m128i result = aes128_prng_next_ ## METHOD(&state);                        \
+		return union_128{.xmm = result}.u128;                                              \
+	}                                                                                      \
+};                                                                                         \
+static_assert(std::uniform_random_bit_generator<aes128_prng_ ## METHOD>);
+
 class aes128_prng_enc_dm_n1
 {
 private:
