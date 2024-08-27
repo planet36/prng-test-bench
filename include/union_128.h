097a704cd588dbaf9f9e46578611133c715da4cb
Steven Ward <planet36@users.noreply.github.com>
2024-08-27 12:09:54 -0400

    Move xmm member to the bottom of the union

--- include/union_128.h
+++ include/union_128.h
@@ -12,9 +12,9 @@ union union_128
 	uint16_t u16[8];
 	uint32_t u32[4];
 	uint64_t u64[2];
-	__m128i xmm;
 #if defined(__SIZEOF_INT128__)
 	__int128_t i128;
 	__uint128_t u128;
 #endif
+	__m128i xmm;
 };
