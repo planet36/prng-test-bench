d2801e7edfd76bc33659ae767ea7a62d020a52b0
Steven Ward <planet36@users.noreply.github.com>
2024-09-13 15:13:04 -0400

    Sync with randp repo

--- include/aes128-utils.h
+++ include/aes128-utils.h
@@ -64,31 +64,53 @@ aes128_dec(__m128i a,
 /// Davies-Meyer single-block-length compression function that uses AES as the block cipher
 /**
 \sa https://en.wikipedia.org/wiki/One-way_compression_function#Davies%E2%80%93Meyer
+\pre \a Nk must be at least \c 1.
 \pre \a Nr must be at least \c 1.
-\param H the previous hash value
-\param m the block of the message
-\param Nr the number of rounds of encryption to perform
-\return the next hash value
+\param a the data
+\param keys the keys
+\param Nk the number of \a keys
+\param Nr the number of rounds of encryption to perform for each key
 */
 static inline __m128i
-aes128_enc_davies_meyer(const __m128i H, const __m128i m, const unsigned int Nr)
+aes128_enc_davies_meyer(__m128i a,
+           const __m128i* keys,
+           const unsigned int Nk,
+           const unsigned int Nr)
 {
-	return _mm_xor_si128(H, aes128_enc(H, &m, 1, Nr));
+	for (unsigned int k = 0; k < Nk; ++k)
+	{
+		for (unsigned int r = 0; r < Nr; ++r)
+		{
+			a = _mm_xor_si128(a, _mm_aesenc_si128(a, keys[k]));
+		}
+	}
+	return a;
 }
 
 /// Davies-Meyer single-block-length compression function that uses AES as the block cipher
 /**
 \sa https://en.wikipedia.org/wiki/One-way_compression_function#Davies%E2%80%93Meyer
+\pre \a Nk must be at least \c 1.
 \pre \a Nr must be at least \c 1.
-\param H the previous hash value
-\param m the block of the message
-\param Nr the number of rounds of decryption to perform
-\return the next hash value
+\param a the data
+\param keys the keys
+\param Nk the number of \a keys
+\param Nr the number of rounds of decryption to perform for each key
 */
 static inline __m128i
-aes128_dec_davies_meyer(const __m128i H, const __m128i m, const unsigned int Nr)
+aes128_dec_davies_meyer(__m128i a,
+           const __m128i* keys,
+           const unsigned int Nk,
+           const unsigned int Nr)
 {
-	return _mm_xor_si128(H, aes128_dec(H, &m, 1, Nr));
+	for (unsigned int k = 0; k < Nk; ++k)
+	{
+		for (unsigned int r = 0; r < Nr; ++r)
+		{
+			a = _mm_xor_si128(a, _mm_aesdec_si128(a, keys[k]));
+		}
+	}
+	return a;
 }
 
 /// Make the packed unsigned 8-bit integers odd.
