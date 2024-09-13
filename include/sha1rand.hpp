887b084b430c12759d968d7b51d82dc10d4daeb6
Steven Ward <planet36@users.noreply.github.com>
2024-09-12 22:23:44 -0400

    Add to the state after it's used for the result

--- include/sha1rand.hpp
+++ include/sha1rand.hpp
@@ -24,9 +24,9 @@ DEF_URBG_CLASS(sha1msg1rand, SINGLE_ARG(std::array<__m128i, 2>), __uint128_t)
 {
 	const __m128i inc = _mm_set_epi64x(xxh_prime64[1], xxh_prime64[0]);
 	const __m128i inc2 = _mm_set_epi64x(xxh_prime64[3], xxh_prime64[2]);
+	const __m128i result = _mm_sha1msg1_epu32(s[0], s[1]);
 	s[0] = _mm_add_epi64(s[0], inc);
 	s[1] = _mm_add_epi64(s[1], inc2);
-	const __m128i result = _mm_sha1msg1_epu32(s[0], s[1]);
 	return union_128{.xmm = result}.u128;
 }
 #pragma GCC diagnostic pop
@@ -37,9 +37,9 @@ DEF_URBG_CLASS(sha1msg2rand, SINGLE_ARG(std::array<__m128i, 2>), __uint128_t)
 {
 	const __m128i inc = _mm_set_epi64x(xxh_prime64[1], xxh_prime64[0]);
 	const __m128i inc2 = _mm_set_epi64x(xxh_prime64[3], xxh_prime64[2]);
+	const __m128i result = _mm_sha1msg2_epu32(s[0], s[1]);
 	s[0] = _mm_add_epi64(s[0], inc);
 	s[1] = _mm_add_epi64(s[1], inc2);
-	const __m128i result = _mm_sha1msg2_epu32(s[0], s[1]);
 	return union_128{.xmm = result}.u128;
 }
 #pragma GCC diagnostic pop
