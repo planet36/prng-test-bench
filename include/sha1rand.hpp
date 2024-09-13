f1324375169e5ac7a6b757b31a6cca17116670e7
Steven Ward <planet36@users.noreply.github.com>
2024-09-12 22:25:53 -0400

    Add comments about unused bits in the SHA functions

--- include/sha1rand.hpp
+++ include/sha1rand.hpp
@@ -24,6 +24,7 @@ DEF_URBG_CLASS(sha1msg1rand, SINGLE_ARG(std::array<__m128i, 2>), __uint128_t)
 {
 	const __m128i inc = _mm_set_epi64x(xxh_prime64[1], xxh_prime64[0]);
 	const __m128i inc2 = _mm_set_epi64x(xxh_prime64[3], xxh_prime64[2]);
+	// XXX: The least significant 64 bits in s[1] are not used.
 	const __m128i result = _mm_sha1msg1_epu32(s[0], s[1]);
 	s[0] = _mm_add_epi64(s[0], inc);
 	s[1] = _mm_add_epi64(s[1], inc2);
@@ -37,6 +38,7 @@ DEF_URBG_CLASS(sha1msg2rand, SINGLE_ARG(std::array<__m128i, 2>), __uint128_t)
 {
 	const __m128i inc = _mm_set_epi64x(xxh_prime64[1], xxh_prime64[0]);
 	const __m128i inc2 = _mm_set_epi64x(xxh_prime64[3], xxh_prime64[2]);
+	// XXX: The least significant 64 bits in s[1] are not used.
 	const __m128i result = _mm_sha1msg2_epu32(s[0], s[1]);
 	s[0] = _mm_add_epi64(s[0], inc);
 	s[1] = _mm_add_epi64(s[1], inc2);
