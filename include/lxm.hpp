6fa94b3628c4c68635bd47fa5e2068ad1786d27b
Steven Ward <planet36@users.noreply.github.com>
2025-09-03 18:12:54 -0400

    Use DEF_URBG_SUBCLASS macro

--- include/lxm.hpp
+++ include/lxm.hpp
@@ -67,75 +67,47 @@ mix_lea(uint64_t h)
 
 }
 
-struct lxm final : public AbstractURBG<std::array<uint64_t, 4>, uint64_t>
+DEF_URBG_SUBCLASS(lxm, SINGLE_ARG(std::array<uint64_t, 4>), uint64_t)
+
+/// prepare the initial state
+void lxm::init()
 {
-protected:
+    // must be odd
+    s[0] |= 1;
 
-    /// prepare the initial state
-    /**
-    * \sa https://github.com/s417-lama/lxm_random/blob/master/include/lxm_random/lxm_random.hpp#L169-L181
-    */
-    void init()
+    // both s[2] and s[3] cannot be 0
+    if ((s[2] == 0) && (s[3] == 0))
     {
-        // must be odd
-        s[0] |= 1;
-
-        // both s[2] and s[3] cannot be 0
-        if ((s[2] == 0) && (s[3] == 0))
-        {
-            const uint64_t v = s[1] + GOLDEN_RATIO_64;
-            s[2] = mix_stafford13(v);
-            s[3] = mix_stafford13(v + GOLDEN_RATIO_64);
-        }
+        const uint64_t v = s[1] + GOLDEN_RATIO_64;
+        s[2] = mix_stafford13(v);
+        s[3] = mix_stafford13(v + GOLDEN_RATIO_64);
     }
+}
 
-public:
+lxm::result_type lxm::next()
+{
+    static constexpr uint64_t M = 0xd1342543de82ef95; // not prime (popcount = 32)
+    static_assert((M & 1) != 0, "must be odd");
 
-    lxm()
-    {
-        init();
-    }
+    static constexpr unsigned int R1 = 24;
+    static constexpr unsigned int R2 = 37;
+    static constexpr unsigned int S1 = 16;
 
-    explicit lxm(const state_type& new_s) : AbstractURBG(new_s)
-    {
-        init();
-    }
+    // Mixing function (lea64)
+    const uint64_t z = mix_lea(s[1] + s[2]);
 
-    explicit lxm(const seed_bytes_type& bytes) : AbstractURBG(bytes)
-    {
-        init();
-    }
+    // Update the LCG subgenerator
+    s[1] = M * s[1] + s[0];
 
-    /**
-    * \sa https://github.com/s417-lama/lxm_random/blob/master/include/lxm_random/lxm_random.hpp#L100-L116
-    */
-    result_type next() override
-    {
-        static constexpr uint64_t M = 0xd1342543de82ef95; // not prime (popcount = 32)
-        static_assert((M & 1) != 0, "must be odd");
+    // Update the XBG subgenerator (xoroshiro128v1_0)
+    uint64_t q0 = s[2];
+    uint64_t q1 = s[3];
+    q1 ^= q0;
+    q0 = std::rotl(q0, R1);
+    q0 ^= q1 ^ (q1 << S1);
+    q1 = std::rotl(q1, R2);
+    s[2] = q0;
+    s[3] = q1;
 
-        static constexpr unsigned int R1 = 24;
-        static constexpr unsigned int R2 = 37;
-        static constexpr unsigned int S1 = 16;
-
-        // Mixing function (lea64)
-        const uint64_t z = mix_lea(s[1] + s[2]);
-
-        // Update the LCG subgenerator
-        s[1] = M * s[1] + s[0];
-
-        // Update the XBG subgenerator (xoroshiro128v1_0)
-        uint64_t q0 = s[2];
-        uint64_t q1 = s[3];
-        q1 ^= q0;
-        q0 = std::rotl(q0, R1);
-        q0 ^= q1 ^ (q1 << S1);
-        q1 = std::rotl(q1, R2);
-        s[2] = q0;
-        s[3] = q1;
-
-        return z;
-    }
-};
-
-static_assert(std::uniform_random_bit_generator<lxm>);
+    return z;
+}
