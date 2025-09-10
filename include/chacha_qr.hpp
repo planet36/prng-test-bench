333d9c041bedab6491c5bc49b64c9d0f981dbfc8
Steven Ward <planet36@users.noreply.github.com>
2025-09-10 11:02:25 -0400

    Add chacha_qr.hpp

---
+++ include/chacha_qr.hpp
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// ChaCha quarter-round function
+/**
+* \file
+* \author Steven Ward
+* \sa https://cr.yp.to/chacha/chacha-20080128.pdf
+* \sa https://datatracker.ietf.org/doc/html/rfc8439#section-2.1
+* \sa https://en.wikipedia.org/wiki/Salsa20#ChaCha_variant
+*/
+
+#pragma once
+
+#include <bit>
+#include <cstdint>
+
+constexpr void
+chacha_qr(uint32_t& a, uint32_t& b, uint32_t& c, uint32_t& d)
+{
+    a += b; d = std::rotl(d ^ a, 16);
+    c += d; b = std::rotl(b ^ c, 12);
+    a += b; d = std::rotl(d ^ a,  8);
+    c += d; b = std::rotl(b ^ c,  7);
+}
