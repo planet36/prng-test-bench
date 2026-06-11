42a912cf3a1e3eb106435aa29b9aaf0a7126db39
Steven Ward <planet36@users.noreply.github.com>
2026-06-11 07:49:03 -0400

    Use auto where possible

--- include/seeds.hpp
+++ include/seeds.hpp
@@ -103,7 +103,7 @@ template <std::uniform_random_bit_generator URBG>
 constexpr auto
 get_seed_bytes_zero()
 {
-    constexpr size_t num_seed_bytes = sizeof(typename URBG::seed_bytes_type);
+    constexpr auto num_seed_bytes = sizeof(typename URBG::seed_bytes_type);
     return std::array<uint8_t, num_seed_bytes>{};
 }
 
@@ -111,7 +111,7 @@ template <std::uniform_random_bit_generator URBG>
 constexpr auto
 get_seed_bytes_pattern()
 {
-    constexpr size_t num_seed_bytes = sizeof(typename URBG::seed_bytes_type);
+    constexpr auto num_seed_bytes = sizeof(typename URBG::seed_bytes_type);
     if constexpr (num_seed_bytes == 4) { return seed_bytes_pattern_4; }
     else if constexpr (num_seed_bytes == 8) { return seed_bytes_pattern_8; }
     else if constexpr (num_seed_bytes == 12) { return seed_bytes_pattern_12; }
