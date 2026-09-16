4bc16b2dcd833bf89a29a20b121cef0e72b7085f
Steven Ward <planet36@users.noreply.github.com>
2026-09-16 17:19:22 -0400

    Instantiate std::random_device locally
    Change std::random_device from data member to local variable in member
    function generate.

--- include/seed_seq.hpp
+++ include/seed_seq.hpp
@@ -41,11 +41,9 @@ struct random_device_seed_seq
     template <std::random_access_iterator Iter>
     void generate(Iter first, Iter last)
     {
+        std::random_device rd;
         std::generate(first, last, std::ref(rd));
     }
-
-private:
-    std::random_device rd;
 };
 
 /// Return an instance of \a PRNG that has been seeded with \c random_device_seed_seq
